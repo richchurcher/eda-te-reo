c234abbc5d29c94c82481a7732bcfd29c5b3ce6d
Rich Churcher <rich.churcher@gmail.com>
2017-01-09 17:26:01 +1300

    Static files

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import koa from 'koa'
+import serve from 'koa-static'
 import koaRouter from 'koa-router'
 import cors from 'kcors'
 import translate from 'google-translate-api'
@@ -8,6 +9,7 @@ const app = new koa()
 const router = koaRouter()
 
 app.use(cors())
+app.use(serve('public'))
 
 const randomIncludeMin = () => Math.floor(Math.random() * proverbs.length)
 
@@ -16,6 +18,10 @@ router.get('/api/proverbs', (ctx, next) => {
 })
 
 router.get('/api/translate', async (ctx, next) => {
+  if (!ctx.query.word) {
+    ctx.body = 'Error: you need to specify a word to translate.'
+    return
+  }
   const translation = await translate(ctx.query.word, {
     from: 'en',
     to: 'mi'
