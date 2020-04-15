ceecfa88cac7f71bc3c6467fd015f5c07a320494
Rich Churcher <rich.churcher@gmail.com>
2020-04-15 12:42:52 +1200

    Upgrade stack, switch translators

--- src/server.js
+++ src/server.js
@@ -1,40 +1,38 @@
-import koa from 'koa'
-import serve from 'koa-static'
-import koaRouter from 'koa-router'
-import cors from 'kcors'
-import translate from 'google-translate-api'
-import proverbs from './proverbs'
+import koa from "koa";
+import serve from "koa-static";
+import koaRouter from "koa-router";
+import cors from "kcors";
+import translate from "@vitalets/google-translate-api";
+import proverbs from "./proverbs";
 
-const app = new koa()
-const router = koaRouter()
+const app = new koa();
+const router = koaRouter();
 
-app.use(cors())
-app.use(serve('public'))
+app.use(cors());
+app.use(serve("public"));
 
-const randomIncludeMin = () => Math.floor(Math.random() * proverbs.length)
+const randomIncludeMin = () => Math.floor(Math.random() * proverbs.length);
 
-router.get('/api/proverbs', (ctx, next) => {
-  ctx.body = proverbs[randomIncludeMin()]
-})
+router.get("/api/proverbs", (ctx, next) => {
+  ctx.body = proverbs[randomIncludeMin()];
+});
 
-router.get('/api/whakatauki', (ctx, next) => {
-  ctx.body = proverbs[randomIncludeMin()]
-})
+router.get("/api/whakatauki", (ctx, next) => {
+  ctx.body = proverbs[randomIncludeMin()];
+});
 
-router.get('/api/translate', async (ctx, next) => {
+router.get("/api/translate", async (ctx, next) => {
   if (!ctx.query.word) {
-    ctx.body = 'Error: you need to specify a word to translate.'
-    return
+    ctx.body = "Error: you need to specify a word to translate.";
+    return;
   }
   const translation = await translate(ctx.query.word, {
-    from: 'en',
-    to: 'mi'
-  })
-  ctx.body = translation.text
-})
+    from: "en",
+    to: "mi",
+  });
+  ctx.body = translation.text;
+});
 
-app
-  .use(router.routes())
-  .use(router.allowedMethods())
+app.use(router.routes()).use(router.allowedMethods());
 
-export default app
+export default app;
